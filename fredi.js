721ae0415202acfdbcd04a39d80de0f13a93e2e5
FeeSpace <frediezra001@gmail.com>
2025-12-20 21:46:18 +0300

    Update fredi.js

--- fredi.js
+++ fredi.js
@@ -9,7 +9,7 @@ require('events').EventEmitter.defaultMaxListeners = 500;
 app.use('/qr', server);
 app.use('/code', code);
 app.use('/pair',async (req, res, next) => {
-res.sendFile(__path + '/fredipair.html')
+res.sendFile(__path + '/pair.html')
 })
 app.use('/',async (req, res, next) => {
 res.sendFile(__path + '/index.html')
